d18de6b18f9be102ab38a880077d59da050bae17
BenS2004 <rbstevenson@cpp.edu>
2026-07-12 13:33:33 -0700

    Refactor BCD counter testbench for clarity
    Updated the testbench to include a checkval variable for improved readability and correctness in BCD comparisons.

--- ECE3300-Lab5-GroupU/simulation/bcd_up_down_counter_tb.v
+++ ECE3300-Lab5-GroupU/simulation/bcd_up_down_counter_tb.v
@@ -34,6 +34,7 @@ module bcd_up_down_counter_tb(
     always #5 clk = ~clk;
     
     integer i, tasksfailed;
+    reg[7:0] checkval;
     initial begin
         clk = 0;
         rst = 0;
@@ -41,17 +42,19 @@ module bcd_up_down_counter_tb(
         tasksfailed = 0;
         for(i=0; i<100; i=i+1)begin
             #10;
-            if(BCD[3:0] != (i % 10)) tasksfailed = tasksfailed + 1;
-            if(BCD[7:4] != (i / 10)) tasksfailed = tasksfailed + 1;            
+            checkval = {(i/10), (i%10)};
+            if(BCD[3:0] != checkval[3:0]) tasksfailed = tasksfailed + 1;
+            if(BCD[7:4] != checkval[7:4]) tasksfailed = tasksfailed + 1;            
         end
             rst = 1;
             #10;
             rst = 0;
-            dir = 0;
+            dir = 1;
         for(i=100; i>=0; i=i-1)begin
             #10;
-            if(BCD[3:0] != (i % 10)) tasksfailed = tasksfailed + 1;
-            if(BCD[7:4] != (i / 10)) tasksfailed = tasksfailed + 1;        
+            checkval = {(i/10), (i%10)};
+            if(BCD[3:0] != checkval[3:0]) tasksfailed = tasksfailed + 1;
+            if(BCD[7:4] != checkval[7:4]) tasksfailed = tasksfailed + 1;        
         end
         if(tasksfailed != 0) $display("%d tasks failed", tasksfailed); else $display("all tasks succeeded");
         $finish;
