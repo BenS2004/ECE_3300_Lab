de7496b926a41677e420f8bf1f402a38b94ef8f8
br0bl <brobles2@cpp.edu>
2026-07-14 22:22:24 -0700

    Update seg7_scan.v
    Only the two 7segs with the digits should be displayed. Changed this and simplified

--- ECE3300-Lab5-GroupU/sources/seg7_scan.v
+++ ECE3300-Lab5-GroupU/sources/seg7_scan.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
-// Engineer: Robert Stevenson
+// Engineer: Robert Stevenson & Ben Robles
 // 
 // Create Date: 07/10/2026 03:10:25 PM
 // Design Name: Seven Segment Display Scan
@@ -15,46 +15,56 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
+// Revision 0.02 - Simplified design
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module seg7_scan(
-    input [7:0] BCD,
-    output wire [7:0] AN,
-    output reg [6:0] SEG,
-    input clk
-    );
+                    input clk,
+                    input rst_n,
+                    input [7:0] BCD,
+                    output reg [7:0] AN,
+                    output reg [6:0] SEG
+                );
     
     reg [19:0] temp;
     reg [3:0] digit;
-    reg [7:0] AN_tmp;
+    
+    wire s;
+    assign s = temp[17];
+    
     always@(posedge clk)
-        temp = temp + 1;
-    wire[2:0] s = temp[19:17];
-       
-    always @(s, BCD) begin
-        case(s)
-           3'b0: digit = BCD[3:0];
-           3'b1: digit = BCD[7:4];
-           default: digit = 0;
-        endcase
+    begin
+        if (!rst_n)
+            temp <= 0;
+        else
+            temp <= temp + 1;
     end
-    
-    always @(s) begin
+        
+    always@(*)
+    begin
         case(s)
-            3'd0:AN_tmp=8'b11111110;3'd1:AN_tmp=8'b11111101;
-            3'd2:AN_tmp=8'b11111011;3'd3:AN_tmp=8'b11110111;
-            3'd4:AN_tmp=8'b11101111;3'd5:AN_tmp=8'b11011111;
-            3'd6:AN_tmp=8'b10111111;3'd7:AN_tmp=8'b01111111;
-            default:AN_tmp=8'b11111111;
-        endcase
+            1'b0: begin
+                digit = BCD[3:0];
+                AN = 8'b11111110;
+            end
+            
+            1'b1: begin
+                digit = BCD[7:4];
+                AN = 8'b11111101;
+            end
+            
+            default: begin
+                digit = 0;
+                AN = 8'b11111111;
+            end     
+        endcase     
     end
-    
-    assign AN = AN_tmp;
-    
-    always @(digit)begin
+        
+    always @(*)
+    begin
         case(digit)
              4'd0: SEG=7'b0000001; 4'd1: SEG=7'b1001111; 4'd2: SEG=7'b0010010;
              4'd3: SEG=7'b0000110; 4'd4: SEG=7'b1001100; 4'd5: SEG=7'b0100100;
@@ -64,4 +74,3 @@ module seg7_scan(
     end
     
 endmodule
-
