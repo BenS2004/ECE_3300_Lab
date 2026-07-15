062a8ef9d4a936d723c78a796394922c8fba6a92
br0bl <brobles2@cpp.edu>
2026-07-14 22:23:39 -0700

    Update bcd_up_down_counter.v
    Changed the implementation to a cascading design to follow the lab

--- ECE3300-Lab5-GroupU/sources/bcd_up_down_counter.v
+++ ECE3300-Lab5-GroupU/sources/bcd_up_down_counter.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
-// Engineer: Robert Stevensom
+// Engineer: Ben Robles
 // 
 // Create Date: 07/10/2026 03:10:25 PM
-// Design Name: BCD Up Down Counter
+// Design Name: Cascading BCD Up Down Counter
 // Module Name: bcd_up_down_counter
 // Project Name: ECE 3300 Group U Lab 5
 // Target Devices: Nexys a7
@@ -15,35 +15,41 @@
 // 
 // Revision:
 // Revision 0.01 - File Created
-// Additional Comments:
+// Revision 0.02 - Adjusted design
+// Additional Comments: 
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module bcd_up_down_counter(
-    input wire clk,
-    input wire rst,
-    input wire dir,
-    output reg [7:0] BCD
+                            input clk,
+                            input rst_n,
+                            input dir,
+                            output wire [7:0] BCD
     );
     
-    reg [8:0] count;   
+    wire [3:0] ones;
+    wire [3:0] tens;
+    wire rollover;
     
-    initial begin count = 0; BCD = 0; end
-    reg[4:0] i;
-    always@(posedge clk)begin
-        if(rst == 1) BCD = 0;
-        else begin
-            if(dir == 0)begin
-                BCD = (BCD + 1) % 8'h9a;
-                if(BCD[3:0] >= 4'b1010) BCD = BCD + 4'b0110;
-            end
-            else begin 
-                BCD = ((BCD + 8'h9a) - 1) % 8'h9a;
-                if(BCD[3:0] == 4'b1111) BCD = BCD - 4'b0110;
-            end            
-        end
-    end             
-    
-endmodule
+    bcd_digit ones_counter(
+                                .clk(clk),
+                                .rst_n(rst_n),
+                                .en(1'b1),
+                                .dir(dir),
+                                .digit(ones),
+                                .rollover(rollover)
+                          );
+                          
+    bcd_digit tens_counter(
+                                .clk(clk),
+                                .rst_n(rst_n),
+                                .en(rollover),
+                                .dir(dir),
+                                .digit(tens),
+                                .rollover()
+                          );
+                          
+  assign BCD = {tens, ones};
 
+endmodule
