fecd18bf87a1a32b54d02d3e2fd28b594d221bb3
br0bl <brobles2@cpp.edu>
2026-07-16 14:36:05 -0700

    Update mux2x1_tb.v
    Fixed small issue: check was done too early

--- ECE3300-Lab5-GroupU/simulation/mux2x1_tb.v
+++ ECE3300-Lab5-GroupU/simulation/mux2x1_tb.v
@@ -22,20 +22,23 @@
 
 module mux2x1_tb(
     
-    );
+                );
+                
     reg a, b, sel;
     wire y;
     integer tasksfailed, tempfailed; 
     reg check;
     reg [1:0] i, j, k, in;
+    
     mux2x1 mux_tb(.a(a), .b(b), .sel(sel), .y(y));
     
     initial begin
-        a <= 0;
-        b <= 0;
-        sel <= 0;
-        tasksfailed <= 0;
-        tempfailed <= 0;
+        a = 0;
+        b = 0;
+        sel = 0;
+        tasksfailed = 0;
+        tempfailed = 0;
+        
         for(i=0; i<2; i=i+1)begin
             sel = i;
             for(j=0; j<2; j=j+1)begin
@@ -43,8 +46,8 @@ module mux2x1_tb(
                 for(k=0; k<2; k=k+1)begin
                     b = k;
                     in = {b, a};
-                    check = y;
                     #10;
+                    check = y;
                     if(check != in[sel]) tasksfailed = tasksfailed + 1;
                     if(tasksfailed > tempfailed) $display("Test failed at %d, %d, %d", i, j, k);
                     tempfailed = tasksfailed;                   
@@ -53,6 +56,4 @@ module mux2x1_tb(
         end
         $finish;
     end
-    
-    
 endmodule
