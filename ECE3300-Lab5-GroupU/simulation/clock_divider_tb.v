c29e44a8c1cae7990f565e9c0b6a423158b9f416
br0bl <brobles2@cpp.edu>
2026-07-16 14:33:33 -0700

    Update clock_divider_tb.v
    Finished

--- ECE3300-Lab5-GroupU/simulation/clock_divider_tb.v
+++ ECE3300-Lab5-GroupU/simulation/clock_divider_tb.v
@@ -1,12 +1,12 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
-// Engineer: Robert Stevensom
+// Engineer: Ben Robles 
 // 
-// Create Date: 07/10/2026 03:23:58 PM
-// Design Name: Clock Divider TestBench
+// Create Date: 07/14/2026 10:40:41 PM
+// Design Name: Clock divider testbench
 // Module Name: clock_divider_tb
-// Project Name: ECE 3300 Lab 5
+// Project Name: ECE3300 Lab 5 Group U
 // Target Devices: Nexys a7
 // Tool Versions: Vivado 2025.2
 // Description: 
@@ -20,20 +20,60 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module clock_divider_tb(
+module clock_divider_tb (
 
-    );
-    reg clk;
-    wire[31:0] counter;
-    reg[32:0] i;
-    clock_divider div_clk(.clk(clk), .clk_div(counter));
-    always #5 clk = ~clk;  
+                        );                     
+
+    reg clk_tb;
+    wire [31:0] clk_div_tb;
+
+    integer i;
+    integer count;  // Expected clk_div_tb count
     
-    initial begin
-        clk = 0;
-        for(i=0; i<33'h0ffffffff; i=i+1)begin
-            #10;
-        end
+    integer dn1, dn2; // delta nibble clk_div_tb[3:0], delta nibble clk_div_tb[7:4]
+    integer clk_prev;   // clk_div_tb prev
+    
+    clock_divider dut (
+                        .clk(clk_tb),
+                        .clk_div(clk_div_tb)
+                      );
+
+    initial begin: Initialize
+        clk_tb = 0;
+        count = 0;
+        dn1 = 0;
+        dn2 = 0;
+        clk_prev = 0;
+    end
+
+    always begin: Clock
+        #5 clk_tb = ~clk_tb;
     end
     
+    initial begin: Testbench
+        #1        
+        for(i = 0; i < 128; i = i + 1) begin
+            clk_prev = clk_div_tb[7:0];         // Set prev
+            
+            @(posedge clk_tb)
+            count = count + 1;                  // Update count after clk edge
+            
+            #1
+            if(count !== clk_div_tb) begin      // Fail if count !== clk_div_tb
+                $display("FAIL  CLK = %b, CLK_DIV = %b", count, clk_div_tb);
+                $fatal;
+            end
+            
+            if(clk_div_tb[3:0] != clk_prev[3:0])    // Track delta nibble 1
+                dn1 = dn1 + 1;
+                
+            if(clk_div_tb[7:4] != clk_prev[7:4])    // Track detla nibble 2
+                dn2 = dn2 + 1;
+        end
+        #5
+        
+        $display("PASS  delta n1 = %d, delta n2 = %d", dn1, dn2);   // Expect dn1 to change 16* faster than dn2
+        $finish; 
+    end
+           
 endmodule
