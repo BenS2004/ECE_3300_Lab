40b1720f5854c14aff5a050ab20c024f5226f048
br0bl <brobles2@cpp.edu>
2026-07-28 19:23:37 -0700

    Update debounce_toggle.v
    Fixed toggle and changed to an asynchronous active low reset

--- ECE3300-Lab7-GroupU/Sources/debounce_toggle.v
+++ ECE3300-Lab7-GroupU/Sources/debounce_toggle.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
-// Engineer: Robert Stevenson
+// Engineer: Robert Stevenson & Ben Robles
 // 
 // Create Date: 07/27/2026 12:49:10 PM
 // Design Name: Debounced Toggle Flip Flop
@@ -13,48 +13,49 @@
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision: 0.02
 // Revision 0.01 - File Created
+// Revision 0.02 - Updated
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module debounce_toggle(
-    input wire btn_raw,
-    input wire clk,
-    input wire rst,
-    output reg data_out
+                            input wire btn_raw,
+                            input wire clk_1kHz,
+                            input wire rst_n,
+                            output reg btn_toggle
     );
     // Debounce
     reg [2:0] btn_val;
     reg btn_clean;
-    initial begin
-        btn_val = 3'b0;
-        btn_clean = 1'b0;
-        data_out = 1'b0;
-    end
+    reg btn_prev;
     
-    always @(posedge clk, posedge rst) begin
-        if(rst == 1) begin
-        btn_val = 3'b000;
-        btn_clean = 1'b0;
-        data_out = 1'b0;        
-        end else begin
+    always @(posedge clk_1kHz or negedge rst_n) begin
+        if(!rst_n) begin
+            btn_val = 3'b000;
+            btn_clean = 1'b0;
+        end
+        else begin
             btn_val = {btn_val[1:0], btn_raw};
-            if(btn_val == 3'b111) begin
-                btn_clean = 1;
-            end else if(btn_val == 3'b000) begin
-                btn_clean = 0;
-            end
+            if(btn_val == 3'b111) btn_clean = 1;
+            else if(btn_val == 3'b000) btn_clean = 0;
         end
     end
     
-    //toggle
-    always @(posedge btn_clean)begin
-        if(rst == 0)begin
-        data_out = ~data_out;
-        end 
+    // Toggle
+    always @(posedge clk_1kHz or negedge rst_n) begin
+        if (!rst_n) begin
+            btn_toggle <= 1'b0;
+            btn_prev <= 1'b0;
+        end
+        else begin
+            if(btn_clean && !btn_prev) 
+            btn_toggle <= ~btn_toggle;
+            
+            btn_prev <= btn_clean;
+        end
     end
     
 endmodule
