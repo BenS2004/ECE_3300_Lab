223733d87e522ffcc43aadc28c9c3715017ce3e1
br0bl <brobles2@cpp.edu>
2026-07-28 18:02:56 -0700

    Update barrel_shifter16.v
    Changed to match assignment implementation with 4 stages and 16 muxes each

--- ECE3300-Lab7-GroupU/Sources/barrel_shifter16.v
+++ ECE3300-Lab7-GroupU/Sources/barrel_shifter16.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
-// Engineer: Robert Stevenson
+// Engineer: Robert Stevenson & Ben Robles
 // 
 // Create Date: 07/27/2026 12:49:10 PM
 // Design Name: 16 Bit Barrel Shifter
@@ -13,60 +13,105 @@
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision: 0.02
 // Revision 0.01 - File Created
+// Revision 0.02 - Updated
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module barrel_shifter16(
-    input wire [15:0] data_in,
-    input wire [3:0] shamt,
-    input wire dir,
-    input wire rot,
-    output wire [15:0] data_out
-    );
-   
-    wire [15:0] level0[0:15], rot_val[0:15], logr_val[0:15], logl_val[0:15];
-    wire [15:0] level1[0:7];
-    wire [15:0] level2[0:3];
-    wire [15:0] level3[0:1];
+                            input [15:0] data_in,
+                            input [3:0] shamt,
+                            input dir,
+                            input rotate,
+                            output wire [15:0] data_out
+                       );
+                   
+    reg [15:0] stage1_in;
+    reg [15:0] stage2_in;
+    reg [15:0] stage3_in;
+    reg [15:0] stage4_in;
     
-    wire [3:0] select;
-    assign select = (shamt & {16{dir}}) | ({16{(~dir)}} & ((16 - shamt)%16));
+    wire [15:0] stage0;
+    wire [15:0] stage1_out;
+    wire [15:0] stage2_out;
+    wire [15:0] stage3_out;
+    wire [15:0] stage4_out;
     
     genvar i;
-        
-    generate
-        assign level0[0] = data_in;
-        
-        for(i = 1; i < 16; i = i + 1)begin    
-            assign rot_val[i] = ({16{rot}} & ({data_in[0+i-1:0], data_in[15:i]}));    
-            assign logr_val[i] = ({16{~rot}}&{16{dir}} & (16'hffff >> i) & {data_in[0+i-1:0], data_in[15:i]});
-            assign logl_val[i] = ({16{~rot}}&{16{~dir}} & (16'hffff << (16 - i)) & {data_in[0+i-1:0], data_in[15:i]});
-            assign level0[i] = rot_val[i] | logr_val[i] | logl_val[i];
-            
-        end               
-        
-        for(i = 0; i < 8; i = i + 1)begin
-            Mux2x1 #(.BUS_SIZE(16)) M1(.a(level0[2*i]), .b(level0[2*i+1]), 
-                .sel(select[0]), .y(level1[i]));
-        end
-        
-        for(i = 0; i < 4; i = i + 1)begin
-            Mux2x1 #(.BUS_SIZE(16)) M2(.a(level1[2*i]), .b(level1[2*i+1]),
-                .sel(select[1]), .y(level2[i]));          
-        end
-        
-        for(i = 0; i < 2; i = i + 1)begin
-            Mux2x1 #(.BUS_SIZE(16)) M3(.a(level2[2*i]), .b(level2[2*i+1]),
-                .sel(select[2]), .y(level3[i]));
-        end
-        
-        Mux2x1 #(.BUS_SIZE(16)) M4(.a(level3[0]), .b(level3[1]),
-           .sel(select[3]), .y(data_out));
-            
-       endgenerate
     
+    assign stage0 = data_in;
+    
+    always @(*)
+    begin
+        case ({rotate, dir})
+            2'b00: begin    // Shift left
+                stage1_in = {stage0[14:0], 1'b0};     
+                stage2_in = {stage1_out[13:0], 2'b00};     
+                stage3_in = {stage2_out[11:0], 4'b0000};    
+                stage4_in = {stage3_out[7:0], 8'b00000000};   
+            end
+            2'b01: begin    // Shift right
+                stage1_in = {1'b0, stage0[15:1]}; 
+                stage2_in = {2'b00, stage1_out[15:2]};     
+                stage3_in = {4'b0000, stage2_out[15:4]};    
+                stage4_in = {8'b00000000, stage3_out[15:8]};
+            end
+            2'b10: begin    // Rotate left
+                stage1_in = {stage0[14:0], stage0[15]}; 
+                stage2_in = {stage1_out[13:0], stage1_out[15:14]};      
+                stage3_in = {stage2_out[11:0], stage2_out[15:12]};     
+                stage4_in = {stage3_out[7:0], stage3_out[15:8]}; 
+            end 
+            2'b11: begin    // Rotate right
+                stage1_in = {stage0[0], stage0[15:1]}; 
+                stage2_in = {stage1_out[1:0], stage1_out[15:2]};      
+                stage3_in = {stage2_out[3:0], stage2_out[15:4]};     
+                stage4_in = {stage3_out[7:0], stage3_out[15:8]};     
+            end
+        endcase
+    end
+    
+    generate
+        for (i = 0; i < 16; i = i + 1) begin
+            mux2x1 mux_stage1(
+                                .a(stage0[i]),
+                                .b(stage1_in[i]),
+                                .sel(shamt[0]),
+                                .y(stage1_out[i])
+                             );
+        end
+        
+        for (i = 0; i < 16; i = i + 1) begin
+            mux2x1 mux_stage2(
+                                .a(stage1_out[i]),
+                                .b(stage2_in[i]),
+                                .sel(shamt[1]),
+                                .y(stage2_out[i])
+                             );
+        end
+        
+        for (i = 0; i < 16; i = i + 1) begin
+            mux2x1 mux_stage3(
+                                .a(stage2_out[i]),
+                                .b(stage3_in[i]),
+                                .sel(shamt[2]),
+                                .y(stage3_out[i])
+                             );
+        end
+        
+        for (i = 0; i < 16; i = i + 1) begin
+            mux2x1 mux_stage4(
+                                .a(stage3_out[i]),
+                                .b(stage4_in[i]),
+                                .sel(shamt[3]),
+                                .y(stage4_out[i])
+                             );
+        end         
+    endgenerate
+                     
+    assign data_out = stage4_out;                     
+                       
 endmodule
