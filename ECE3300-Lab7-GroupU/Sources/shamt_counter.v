7f1835e8946947ca82b42b26d82708b2f6bd23d8
br0bl <brobles2@cpp.edu>
2026-07-28 19:37:25 -0700

    Update shamt_counter.v
    Changed to use existing debounce_toggle module

--- ECE3300-Lab7-GroupU/Sources/shamt_counter.v
+++ ECE3300-Lab7-GroupU/Sources/shamt_counter.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
 // Company: Cal Poly Pomona
-// Engineer: Robert Stevenson
+// Engineer: Robert Stevenson & Ben Robles
 // 
 // Create Date: 07/27/2026 12:49:10 PM
 // Design Name: Shift Amount Counter
@@ -13,40 +13,34 @@
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision: 0.02
 // Revision 0.01 - File Created
+// Revision 0.02 - Updated
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module shamt_counter(
-    input wire btn_raw,
-    input wire clk,
-    output reg btn_clean,
-    output reg[1:0] amt_out
-    );
-    //debounce
-    reg[2:0] btn_val;    
+                        input wire clk_1kHz,
+                        input wire rst_n,
+                        input wire btn_toggle,
+                        output reg [1:0] shamt_high
+                    );
+
+    reg btn_prev;
     
-    initial begin
-        btn_val = 3'b000;
-        btn_clean = 1'b0;
-        amt_out = 2'b00;
-    end
-    
-    always @(posedge clk)begin
-        btn_val = {btn_val[1:0], btn_raw};
-        if(btn_val == 3'b000)begin
-            btn_clean = 0;
-        end else if(btn_val == 3'b111)begin
-            btn_clean = 1;
+    always @(posedge clk_1kHz or negedge rst_n) begin
+        if(!rst_n) begin
+            shamt_high <= 2'b00;
+            btn_prev <= 1'b0;
+        end
+        else begin
+            btn_prev <= btn_toggle;
+            
+            if(btn_toggle != btn_prev)
+                shamt_high <= shamt_high + 2'b01;
         end
     end
-    
-    //increment amt
-    
-    always @(posedge btn_clean)begin
-        amt_out = amt_out + 1;
-    end
+
 endmodule
