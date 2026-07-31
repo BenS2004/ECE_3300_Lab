04ca32aaadaf00ddbad1b8ade907479990ac35b5
br0bl <brobles2@cpp.edu>
2026-07-30 19:11:04 -0700

    Update debounce_toggle_tb.v

--- ECE3300-Lab7-GroupU/Simulation/debounce_toggle_tb.v
+++ ECE3300-Lab7-GroupU/Simulation/debounce_toggle_tb.v
@@ -1,80 +1,124 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: Ben Robles
+// Engineer: Cal Poly Pomona
 // 
 // Create Date: 07/30/2026 02:18:52 PM
-// Design Name: 
+// Design Name: Debounce & Toggle Testbench
 // Module Name: debounce_toggle_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
+// Project Name: ECE3300_Lab7_GroupU
+// Target Devices: Nexys a7
+// Tool Versions: Vivado 2025.2
 // Description: 
 // 
 // Dependencies: 
 // 
-// Revision:
+// Revision: 
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
 
+
 module debounce_toggle_tb(
 
-    );
+                         );
+                         
+    reg clk_tb;               
+    reg btn_raw_tb;
+    reg rst_n_tb;
+    wire btn_toggle_tb;
     
-    reg raw_in, rst_n, clk;
-    wire out;
+    debounce_toggle db_tb(
+                    .clk_1kHz(clk_tb), 
+                    .btn_raw(btn_raw_tb),
+                    .rst_n(rst_n_tb),
+                    .btn_toggle(btn_toggle_tb)
+                  );
     
-    debounce_toggle TB(.btn_raw(raw_in), 
-        .clk_1kHz(clk), .rst_n(rst), .btn_toggle(out));
-    
-    initial begin
-        clk = 1;
-        forever #5 clk = ~clk;       
-    end
-    
-    
-    
-    reg[2:0] series_in, i;
-    initial begin
-        rst = 1;
-        raw_in = 0;
-        series_in = 3'b000;
-        #10;
-        rst = 0;
-        repeat(8) begin
-            for(i = 0; i < 3; i = i + 1)begin
-                raw_in = series_in[i];
-                #10;
-                series_in = series_in + 1;
-            end
+    initial 
+        begin
+            clk_tb = 1'b0;
+            rst_n_tb = 1'b0;
         end
-        rst = 1;
-        #10;       
-        series_in = 0;
-            for(i = 0; i < 3; i = i + 1)begin
-                raw_in = series_in[i];
-                #10;
-            end
-        #10;
-        series_in = 7;
-            for(i = 0; i < 3; i = i + 1)begin
-                raw_in = series_in[i];
-                #10;
-            end
-        #10;
-        series_in = 0;
-        repeat(8) begin
-            for(i = 0; i < 3; i = i + 1)begin
-                raw_in = series_in[i];
-                #10;
-                series_in = series_in + 1;
-            end
-        end   
-             
-        $finish;
-    end
     
+    always
+        begin  
+            #5 clk_tb = ~clk_tb;
+         end
+         
+     initial
+        begin
+            @(posedge clk_tb)
+            #1;
+            rst_n_tb = 1'b1;
+          
+            btn_raw_tb = 1'b0;                // Test 1: Stable low     
+            repeat(6) @(posedge clk_tb);
+            #1;
+            
+            if(btn_toggle_tb !== 1'b0)
+            begin
+                $display("FAIL: btn_toggle should be 0 after stable low");
+                $fatal;
+            end     
+                    
+            btn_raw_tb = 1'b1;               // Test 2: Bounce  
+            @(posedge clk_tb);
+            #1;
+            
+            btn_raw_tb = 1'b0;  
+            @(posedge clk_tb);
+            #1;
+            
+            if(btn_toggle_tb !== 1'b0)
+            begin
+                $display("FAIL: btn_toggle changed after bounce");
+                $fatal;
+            end        
+            
+            btn_raw_tb = 1'b1;              // Test 3: Toggle on
+            repeat(6) @(posedge clk_tb);
+            #1;   
+            btn_raw_tb = 1'b0;
+            repeat(6) @(posedge clk_tb);
+            
+            if(btn_toggle_tb !== 1'b1)
+            begin
+                $display("FAIL: btn_toggle should be 1 after stable high");
+                $fatal;
+            end
+            
+            btn_raw_tb = 1'b1;               // Test 4: Bounce  
+            @(posedge clk_tb);
+            #1;
+            
+            btn_raw_tb = 1'b0;  
+            @(posedge clk_tb);
+            #1;
+            
+            if(btn_toggle_tb !== 1'b1)
+            begin
+                $display("FAIL: btn_toggle changed after bounce");
+                $fatal;
+            end    
+            
+            btn_raw_tb = 1'b1;              // Test 3: Toggle off
+            repeat(6) @(posedge clk_tb);
+            #1;   
+            btn_raw_tb = 1'b0;
+            repeat(6) @(posedge clk_tb);
+            
+            if(btn_toggle_tb !== 1'b0)
+            begin
+                $display("FAIL: btn_toggle should be 0 after stable high");
+                $fatal;
+            end     
+            
+            $display("Passed all tests");
+            $finish;
+        end
+
+
 endmodule
